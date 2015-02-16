d29ad4ec0fa4d379485fee780f58d7c652d45613
wesperinhell@gmail.com <wesperinhell@gmail.com@e98b7d98-9c72-b875-93af-b59096ed7f56>
2015-02-16 19:38:11 +0000

    Вадим (1.00.4) Отчет "Расчет стоимости заработной платы рабочих строителей"

--- trunk/Sources/dmReportU.pas
+++ trunk/Sources/dmReportU.pas
@@ -50,8 +50,21 @@ Uses DataModule;
 
 // отчет Расчет стоимости заработной платы рабочих строителей (Вадим)
 procedure TdmReportF.Report_ZP_OBJ(SM_ID: integer; FileReportPath: string);
+var
+  SM_ID_0: integer;
 begin
   frxReport.LoadFromFile(FileReportPath + 'frZP_OBJ.fr3');
+
+
+  qrTMP.SQL.Text := 'SELECT COALESCE(ssd_2.SM_ID, ssd_1.SM_ID, ssd.SM_ID) as SM_ID_0'#13#10 +
+                    'FROM smetasourcedata ssd'#13#10 +
+                    'LEFT JOIN smetasourcedata ssd_1 ON ssd_1.SM_ID = (ssd.PARENT_LOCAL_ID + ssd.PARENT_PTM_ID)'#13#10 +
+                    'LEFT JOIN smetasourcedata ssd_2 ON ssd_2.SM_ID = (ssd_1.PARENT_LOCAL_ID + ssd_1.PARENT_PTM_ID)'#13#10 +
+                    'WHERE ssd.sm_id = :SM_ID';
+  qrTMP.ParamByName('SM_ID').AsInteger := SM_ID;
+  CloseOpen(qrTMP);
+  SM_ID_0 := qrTMP.FieldByName('SM_ID_0').AsInteger;
+  qrTMP.Close;
 
   qrTMP.SQL.Text := 'SELECT CONCAT(oc.NUM, " ", oc.NAME) as Name, IFNULL(s.MONAT, 0) as Month, IFNULL(s.YEAR, 0) as Year,'#13#10 +
                     '       IF(os.OBJ_REGION = 3, s.STAVKA_M_RAB, s.STAVKA_RB_RAB) as Tarif,'#13#10 +
@@ -61,11 +74,11 @@ begin
                     'LEFT JOIN objstroj as os ON os.STROJ_ID = oc.STROJ_ID'#13#10 +
                     'LEFT JOIN stavka as s ON s.STAVKA_ID = ssd.STAVKA_ID'#13#10 +
                     'WHERE ssd.SM_ID = :SM_ID';
-  qrTMP.ParamByName('SM_ID').AsInteger := SM_ID;
+  qrTMP.ParamByName('SM_ID').AsInteger := SM_ID_0;
   CloseOpen(qrTMP);
 
   qrZP_OBJ.SQL.Text := 'call smeta.Report_ZP_OBJ(:SM_ID, :MONTH, :YEAR)';
-  qrZP_OBJ.ParamByName('SM_ID').AsInteger := SM_ID;
+  qrZP_OBJ.ParamByName('SM_ID').AsInteger := SM_ID_0;
   qrZP_OBJ.ParamByName('MONTH').AsInteger := qrTMP.FieldByName('Month').AsInteger;
   qrZP_OBJ.ParamByName('YEAR').AsInteger := qrTMP.FieldByName('Year').AsInteger;
   CloseOpen(qrZP_OBJ);
